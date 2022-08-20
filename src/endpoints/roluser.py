6473f9397e2ae0adc0eea74c476c2bd51daf3a11
Brayan Florez F <brayan.florez@binarylatam.com>
2022-08-20 10:08:13 -0500

    Cambios Rol 2

--- src/endpoints/roluser.py
+++ src/endpoints/roluser.py
@@ -15,21 +15,21 @@ logger = logging.getLogger(__name__)  # the __name__ resolve to "uicheckapp.serv
 
 #APIRouter creates path operations for user module
 router = APIRouter(
-    prefix="/roluser",
-    tags=["roluser"],
+    prefix="/roleUsers",
+    tags=["roleUsers"],
     responses={404: {"description": "Not found"}},
 )
 
 @router.get("/")
 async def read_root():
-    url = 'https://6300e253e71700618a30685c.mockapi.io/roleUsers/roleUsers'
+    url = 'https://6300f6cae71700618a320ab0.mockapi.io/roleUsers/roleUsers'
     response = requests.get(url, {}, timeout=5)
     logger.info("Hola Rol usuarios")
     return {"encryptedToken": response.json() }
 
 @router.get("/{encryptedToken}")
 async def read_user(encryptedToken: str):
-    url = 'https://6300e253e71700618a30685c.mockapi.io/roleUsers/roleUsers'+encryptedToken
+    url = 'https://6300f6cae71700618a320ab0.mockapi.io/roleUsers/roleUsers'+encryptedToken
     response = requests.get(url, {}, timeout=5)
     if(response.status_code==  500):
      raise HTTPException(status_code=204, detail="Item not found")
@@ -39,16 +39,16 @@ async def read_user(encryptedToken: str):
 
 @router.post("/add")
 async def add_user(rol: RolUserModel):
-    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'
-    params = {'RolUserModel':rol}
-    response = requests.post(url, params = params, timeout=5)
+    url = 'https://6300f6cae71700618a320ab0.mockapi.io/roleUsers/rolUsers/'
+    data=json.loads(rol.json())
+    response = requests.put(url, data = data, timeout=5)
     jsonresponse = json.loads(response.text)
     return jsonresponse
 
 
 @router.put("/update/{idUsuario}")
 async def read_user(rol: RolUserModel,encryptedToken: str):
-    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+encryptedToken
+    url = 'https://6300f6cae71700618a320ab0.mockapi.io/roleUsers/rolUsers/'+encryptedToken
     data=json.loads(rol.json())
     response = requests.put(url, data = data, timeout=5)
     if(response.status_code==  404):
@@ -59,7 +59,7 @@ async def read_user(rol: RolUserModel,encryptedToken: str):
 
 @router.delete("/delete/{encryptedToken}")
 async def read_user(encryptedToken: str):
-    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+encryptedToken
+    url = 'https://6300f3d19a1035c7f8fb507f.mockapi.io/rolUsers/rolUsers/'+encryptedToken
     response = requests.delete(url)
     if(response.status_code==  404):
      raise HTTPException(status_code=204, detail="Item not found")
--- src/models/roluser.py
+++ src/models/roluser.py
@@ -1,6 +1,8 @@
+from datetime import date
 from pydantic import BaseModel, Field
 from typing import Optional
 
 class RolUserModel(BaseModel):
     encryptedToken: Optional[str] = ''
     role:  Optional[str] = ''
+    expirationDate:  Optional[date] = date.today()
